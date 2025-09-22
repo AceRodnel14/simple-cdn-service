1724fcf7fc112777c8f59f68a155e64a44c6bc9a
“k8scodeace” <48496511+AceRodnel14@users.noreply.github.com>
2025-09-22 12:26:05 +0800

    Updated script to allow multiple LANs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
-envsubst '${SERVER_NAME} ${ALLOWED_LAN}' < /etc/nginx/nginx.conf.template > /etc/nginx/conf.d/default.conf
+ALLOWED_BLOCKS=""
+for net in $ALLOWED_LAN; do
+    ALLOWED_BLOCKS="${ALLOWED_BLOCKS}        allow $net;\n"
+done
+
+# Export with printf to interpret \n as actual newlines
+export ALLOWED_BLOCKS=$(printf "$ALLOWED_BLOCKS")
+
+envsubst '${SERVER_NAME} ${ALLOWED_BLOCKS}' \
+    < /etc/nginx/nginx.conf.template \
+    > /etc/nginx/conf.d/default.conf
 
 exec nginx -g 'daemon off;'
